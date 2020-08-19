7caf464add3d951279ec73eb42f01ac705d93796
Arjun Ghimire <developerarjun1@gmail.com>
2020-08-19 22:03:07 +0545

    make

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/' + process.env.CI_PROJECT_NAME + '/'
-    : '/'
+  presets: ["@vue/cli-plugin-babel/preset"]
 };
